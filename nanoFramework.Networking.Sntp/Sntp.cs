0dfd0c150495970c287a08496492a9f675c68708
Robin Jones <networkfusion@users.noreply.github.com>
2023-04-04 14:20:51 +0100

    Update nanoFramework.Networking.Sntp/Sntp.cs
    Co-authored-by: José Simões <jose.simoes@eclo.solutions>

--- nanoFramework.Networking.Sntp/Sntp.cs
+++ nanoFramework.Networking.Sntp/Sntp.cs
@@ -74,7 +74,7 @@ namespace nanoFramework.Networking
 
         /// <summary>
         /// Gets, sets the host address of the backup SNTP server.
-        /// By default it uses the server '1.pool.ntp.org'
+        /// By default it uses the server '1.pool.ntp.org'.
         /// </summary>
         /// <remarks>
         /// For a list of some public NTP servers, see this link :
